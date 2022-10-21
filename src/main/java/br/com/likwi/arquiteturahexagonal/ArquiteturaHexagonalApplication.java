7f723cce466e5cd5a46eeccf70fa6213822f644c
Renato Fagalde <renato@likwi.com.br>
2022-10-20 22:40:23 -0300

    inicializando o reposótio

---
+++ src/main/java/br/com/likwi/arquiteturahexagonal/ArquiteturaHexagonalApplication.java
@@ -0,0 +1,13 @@
+package br.com.likwi.arquiteturahexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturaHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiteturaHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/likwi/arquiteturahexagonal/ArquiteturaHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.likwi.arquiteturahexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturaHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
